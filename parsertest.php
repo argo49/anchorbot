9df835c4c7ea4e742e93b67464c6d0549f4172a1
Joseph Szymborski <joseph.szymborski@naj-p.com>
2013-11-09 04:45:00 -0500

    Made the Scraper suck a whole lot less

--- parsertest.php
+++ parsertest.php
@@ -4,8 +4,11 @@
 	require_once 'lib/php/alchemyapi.php';
     $alchemyapi = new AlchemyAPI();
 	
-	$responseKeywords = $alchemyapi->entities('url', "http://www.cnn.com/2013/11/08/us/fort-meade-sexual-abuse-investigation/index.html?hpt=hp_t2", array("showSourceText"=>1));
+	$responseKeywords = $alchemyapi->keywords('url', "http://www.cnn.com/2013/11/08/us/fort-meade-sexual-abuse-investigation/index.html?hpt=hp_t2", array("maxRetrieve"=>6));
 
-	json_encode($responseKeywords);
+	foreach($responseKeywords["keywords"] as $keyword){
+		var_dump($keyword["text"]);
+	}
+	//var_dump(json_encode($responseKeywords["keywords"]));
 ?>
 </pre>
